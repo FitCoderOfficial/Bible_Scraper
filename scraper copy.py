319ad0f02b558015a7065245e1d0180c6b085c12
Shin-Sae-byeok <toqur1219@naver.com>
2020-07-23 02:59:32 +0900

    'feat:chapter'

--- scraper copy.py
+++ scraper copy.py
@@ -5,22 +5,39 @@ import time
 import json
 import os
 
+# Chrome의 경우 | 아까 받은 chromedriver의 위치를 지정해준다.
+driver = webdriver.Chrome('D:\Works\PG_Works\Bible_Scraper\chromedriver')
+# 암묵적으로 웹 자원 로드를 위해 3초까지 기다려 준다.
+driver.implicitly_wait(3)
 
-req = requests.get('https://wol.jw.org/ko/wol/b/r8/lp-ko/nwtsty/1/1#study=discover')
-html = req.text
-soup = BeautifulSoup(html, 'html.parser')
+
+
+# url에 접근한다.
+driver.get('https://wol.jw.org/ko/wol/b/r8/lp-ko/nwt/1/50#study=discover')
+#driver.get('https://wol.jw.org/en')
+
+
+
+# #현재 링크 확인
+current_link = driver.current_url
+
+# req = requests.get('https://wol.jw.org/ko/wol/b/r8/lp-ko/nwtsty/1/50#study=discover')
+# html = req.text
+# soup = BeautifulSoup(html, 'html.parser')
 #article_list = soup.find('div', {'id':'article'})
 #article_list = soup.findAll('span', {'class':'tt vl'})
 
-article_list = soup.select('article > p > span')
 
-f = open('test3.txt', 'w', encoding='utf-8')
-for i in article_list:
-    f.write(i.get_text()+'\n')
 
-f.close()    
+# class chapter_scraper:
+#     article_list = soup.select('article > p > span')
+#     f = open('test4.txt', 'w', encoding='utf-8')
+#     for i in article_list:
+#         f.write(i.get_text()+'\n')
+#     f.close()
 
-print(article_list)
+
+# chapter_scraper
 
 
 # f = open('test3.txt', 'w', encoding='utf-8')
@@ -35,3 +52,16 @@ print(article_list)
 
 
  
+# class group_scraping:
+#     while True:
+#         if driver.find_elements_by_css_selector('#publicationNavigation > div > ul > li.resultNavRight.disabled'):
+#             back_to_menu = driver.find_elements_by_css_selector('#menuBible')[0].click()
+#             break
+#         else:
+#             go_to_next = driver.find_elements_by_css_selector('#publicationNavigation > div > ul > li.resultNavRight')[0].click()
+
+# group_scraping
+
+
+title = driver.find_elements_by_css_selector('#article > article > header > h1')[0].get_text()
+print(title)
--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ import json
 import os
 
 # Chrome의 경우 | 아까 받은 chromedriver의 위치를 지정해준다.
-driver = webdriver.Chrome('D:\Works\PG_Works\scraper\chromedriver')
+driver = webdriver.Chrome('D:\Works\PG_Works\Bible_Scraper\chromedriver')
 # 암묵적으로 웹 자원 로드를 위해 3초까지 기다려 준다.
 driver.implicitly_wait(3)
 
@@ -15,11 +15,11 @@ driver.implicitly_wait(3)
 driver.get('https://wol.jw.org/ko')
 #driver.get('https://wol.jw.org/en')
 
-# id="menuPublications" 인 값을 찾는다
+# # id="menuPublications" 인 값을 찾는다
 element = driver.find_element_by_id("menuPublications").click()
 time.sleep(1)
 
-# 리스트 값중에 첫번째 값을 찾는다
+# # 리스트 값중에 첫번째 값을 찾는다
 Publications_list = driver.find_elements_by_css_selector('#article > article > div > nav > ul > li')[0].click()
 time.sleep(1)
 
@@ -27,29 +27,69 @@ time.sleep(1)
 bible_list = driver.find_elements_by_css_selector('#article > article > div > nav > ul > li')[0].click()
 time.sleep(1)
 
-
 bible_start = driver.find_elements_by_class_name('bookLink')[0].click()
 time.sleep(1)
 
 chapter_start = driver.find_elements_by_css_selector('#article > article > div > nav > div > ul.grid.chapters.clearfix > li')[0].click()
 time.sleep(1)
 
-#현재 링크 확인
+# #현재 링크 확인
 current_link = driver.current_url
 
-req = requests.get(current_link)
-html = req.text
-soup = BeautifulSoup(html, 'html.parser')
 
-article_list = soup.select('article > p > span')
+# chapter_next = driver.find_elements_by_css_selector('#publicationNavigation > div > ul > li.resultNavRight')[0].click()
+# chapter_next
+# print(chapter_next)
 
-f = open('test1.txt', 'w', encoding='utf-8')
-for i in article_list:
-    f.write(i.get_text()+'\n')
+#publicationNavigation > div.chrome.forwardBackNavControls.resultNavControls.meps-lang-KO.meps-script-KOREAN > ul > li.resultNavRight.disabled
 
-f.close()    
+# class chapter_save:
+#     req = requests.get(current_link)
+#     html = req.text
+#     soup = BeautifulSoup(html, 'html.parser')
+#     article_list = soup.select('article > p > span')
+#     f = open('test{}.txt'.format(number), 'w', encoding='utf-8')
+#     for i in article_list:
+#         f.write(i.get_text()+'\n')
+#     f.close()
 
-print(article_list)
+
+class group_scraping:
+
+    while True:
+        number=1
+        if driver.find_elements_by_css_selector('#publicationNavigation > div > ul > li.resultNavRight.disabled'):
+            current_link = driver.current_url
+            title = driver.find_elements_by_css_selector('#article > article > header > h1')[0].text.strip()
+            req = requests.get(current_link)
+            html = req.text
+            soup = BeautifulSoup(html, 'html.parser')
+            article_list = soup.select('article > p > span')
+            f = open('{}{0}.txt'.format(title, number), 'w', encoding='utf-8')
+            for i in article_list:
+                f.write(i.get_text()+'\n')
+            f.close()
+            back_to_menu = driver.find_elements_by_css_selector('#menuBible')[0].click()
+            number=1
+            break
+        else:
+            current_link = driver.current_url
+            title = driver.find_elements_by_css_selector('#article > article > header > h1')[0].text.strip()
+            req = requests.get(current_link)
+            html = req.text
+            soup = BeautifulSoup(html, 'html.parser')
+            article_list = soup.select('article > p > span')
+            f = open('{}{0}.txt'.format(title, number), 'w', encoding='utf-8')
+            for i in article_list:
+                f.write(i.get_text()+'\n')
+            f.close()
+            number+=1
+            go_to_next = driver.find_elements_by_css_selector('#publicationNavigation > div > ul > li.resultNavRight')[0].click()
+
+group_scraping
+
+
+
 
 
 
